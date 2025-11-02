715b33e362baf4cf540a0140e45060e507cca037
Swetang Pandit <swetangpandit@gmail.com>
2025-11-02 22:29:38 +1000

    [Phase 3]Problem Statment and other files templet.

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info() # Not getting 1st 2 values but need 2nd value only from exc_info()
@@ -19,3 +19,10 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Division by zero error occurred")
+        raise CustomException(e, sys)
