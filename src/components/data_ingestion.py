5bc6fc9f80620d6bee7d1909768b1947e59c14d0
Swetang Pandit <swetangpandit@gmail.com>
2025-11-16 21:45:32 +1100

    [Phase 6] Data ingestion with csv.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,10 +26,34 @@ class DataIngestion:
 
         # Read the data from source
         try:
+            # Reding the dataset from the csv file
             df = pd.read_csv('notebooks/data/stud.csv')
             logging.info("Read the datasets")
 
-            
+            # Create the artifacts directory if it doesn't exist
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-        except:
-            pass
+            logging.info("Train test split initiated")
+
+
+            # Split the data into train and test sets
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            
+            # Save the train and test sets to their respective paths
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            # Return the paths of the train and test data
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
